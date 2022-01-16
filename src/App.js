dd1449ae04e885656f91ff1921bc2421c0d0422b
Martin Semerad <martin.sem84@gmail.com>
2022-01-16 04:20:25 +0100

    done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ export default function App() {
   const [ready, setReady] = useState(false);
   const [loading, setLoading] = useState(false);
   const [meta, setMeta] = useState({ fileName: "track.mp3", title: "track" });
+  const [done, setDone] = useState([]);
+  const [programTitle, setProgramTitle] = useState("");
 
+  console.log(done);
   const reset = () => {
     setReady(false);
     setBlob(null);
@@ -34,6 +37,7 @@ export default function App() {
     if (!link) {
       return;
     }
+    setDone([]);
     setLoading(true);
     try {
       const res = await fetch(link);
@@ -45,16 +49,21 @@ export default function App() {
         const img = picture.getAttribute("data-srcset");
         setImage(img || "");
       }
+      const content = doc.querySelector(".content");
+      if (content) {
+        const h1 = content.querySelector("h1");
+        const programTitle = h1.textContent;
+        setProgramTitle(programTitle || "");
+      }
       const playlist = doc.querySelector(".sm2-playlist-wrapper");
       if (playlist) {
         const tracks = Array.from(playlist.querySelectorAll("a"));
         const info = tracks.map((track, index) => {
           const href = track.getAttribute("href").split("?uuid=")[0];
           const title = track.querySelector("div > div").textContent;
-          const fileName = `${String(index + 1).padStart(
-            2,
-            "0"
-          )}_${title.replaceAll(/[: .]+/g, "-").toLowerCase()}`;
+          const fileName = `${String(index + 1).padStart(2, "0")}_${title
+            .replaceAll(/[: .]+/g, "-")
+            .toLowerCase()}`;
           return { href, fileName, title };
         });
         setTracks(info);
@@ -74,10 +83,12 @@ export default function App() {
       {loading && (
         <div className="overlay">
           <div className="content">
-            <div>
-              <img src="/skate.gif" alt="loading..." />
+            <div className="content-card">
+              <div>
+                <img src="/skate.gif" alt="loading..." />
+              </div>
+              <div className="loading-text">Malý moment. Láduju to tam.</div>
             </div>
-            <div className="loading-text">Malý moment. Láduju to tam.</div>
           </div>
         </div>
       )}
@@ -100,47 +111,56 @@ export default function App() {
           Načíst
         </button>
       </form>
-      {!loading && tracks.length > 0 && (
-        <div>
+      {tracks.length > 0 && (
+        <div className="program-title">
+          <div className="title-wrapper">
+            <h2>{programTitle}</h2>
+          </div>
           <div className="preview">
             <img srcSet={image} alt="preview" />
           </div>
-          {tracks.map((track, i) => (
-            <div key={track.fileName}>
-              <div className="item">
-                <span>
-                  <b>{`[${i + 1}] `}</b>
-                  {track.title}{" "}
-                </span>
-                <button
-                  disabled={loading}
-                  onClick={() => {
-                    loadBlob(track.href, track.fileName, track.title);
-                  }}
-                >
-                  {` > `}
-                </button>
-              </div>
-            </div>
-          ))}
         </div>
       )}
+      <div>
+        {tracks.map((track, i) => (
+          <div key={track.fileName}>
+            <div className="item">
+              <span>
+                <b>{`[${i + 1}] `}</b>
+                {track.title}{" "}
+              </span>
+              <button
+                className="confirm"
+                disabled={loading}
+                onClick={() => {
+                  loadBlob(track.href, track.fileName, track.title);
+                  setDone((prev) => [...prev, i]);
+                }}
+              >
+                {done.includes(i) ? "✓" : ">"}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
       {ready && (
         <div className="overlay">
           <div className="content">
-            <div className="close">
-              <button onClick={reset}>Zavřít</button>
-            </div>
-            <div>
-              <div className="title">{meta.title}</div>
+            <div className="content-card">
+              <div className="close">
+                <button onClick={reset}>✕</button>
+              </div>
               <div>
-                <a
-                  href={blob}
-                  onClick={() => setReady(false)}
-                  download={`${meta.fileName}.mp3`}
-                >
-                  Stáhnout track
-                </a>
+                <div className="title">{meta.title}</div>
+                <div>
+                  <a
+                    href={blob}
+                    onClick={() => setReady(false)}
+                    download={`${meta.fileName}.mp3`}
+                  >
+                    Uložit
+                  </a>
+                </div>
               </div>
             </div>
           </div>
